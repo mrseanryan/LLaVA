646baf1ef8f7764967dacb39de4285a71d6ebf77
Sean Ryan <sean.ryan@mendix.com>
2024-02-19 14:44:43 +0100

    Adjust train for 1.5

--- train_qlora_v1.5-llama-sr.sh
+++ train_qlora_v1.5-llama-sr.sh
@@ -2,19 +2,23 @@
 
 set -e -x  # stop on 1st error, debug output of args used
 
-deepspeed llava/train/train_mem.py \
+# --pretrain_mm_mlp_adapter - added this, since the output misses this file, and this option is used in the other example tuning scripts.
+#   but what is the correct value?
+
+deepspeed ./llava/train/train_mem.py \
+    --deepspeed ./scripts/zero2.json \
     --lora_enable True \
-	--lora_r 128 \
-	--lora_alpha 256 \
-	--mm_projector_lr 2e-5 \
+    --lora_r 128 \
+    --lora_alpha 256 \
+    --mm_projector_lr 2e-5 \
     --bits 4 \
-    --deepspeed ./scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
     --data_path ./temp/dataset/train/dataset.json \
     --validation_data_path ./temp/dataset/validation/dataset.json \
     --image_folder ./temp/dataset/images/ \
     --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter ./temp/llava-v1.5-7b/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -22,12 +26,12 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-task-qlora \
-    --num_train_epochs 1 \
+    --output_dir ./temp/checkpoints/llava-v1.5-7b-task-qlora \
+    --num_train_epochs 10 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
+    --gradient_accumulation_steps 2 \
+    --evaluation_strategy "epoch" \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
@@ -40,5 +44,4 @@ deepspeed llava/train/train_mem.py \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
-    --lazy_preprocess True \
-    --report_to xxx
+    --lazy_preprocess True
